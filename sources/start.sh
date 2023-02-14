74bce291cb5ce8a701a3c64c4972e8854727afc3
debockpierrecofely <pierre.debock@equans.com>
2023-02-14 15:09:32 +0100

    SavePoint

---
+++ sources/start.sh
@@ -0,0 +1,30 @@
+export ELK_URL="airnyx.cofelygtc.com"
+export ELK_LOGIN="user"
+export ELK_PASSWORD="aimelespetitslapins"
+export ELK_PORT=9200
+export ELK_SSL=true
+export LOCAL_MODE=true
+export UIVERSION="v3.25.193"
+
+export REDIS_IP="airnyx.cofelygtc.com"
+export USE_LOGSTASH=true
+export AMQC_URL="airnyx.cofelygtc.com"
+export AMQC_LOGIN=admin
+export AMQC_PASSWORD=nyxaimelespetitslapins
+export AMQC_PORT=61613
+
+
+export OUTPUT_FOLDER=./outputs/
+export OUTPUT_URL=https://airnyx.cofelygtc.com/generatedreports/rapi/
+export TZ=Europe/Paris
+export WELCOMEMESSAGE=NYX AWS Platform
+export ICON=breadexportslice
+
+export PG_LOGIN=nyx
+export PG_PASSWORD=postgrespass
+export PG_HOST="airnyx.cofelygtc.com"
+export PG_PORT=5432
+export PG_DATABASE=nyx
+export CLIENT=AIRNYX
+
+python3 nyx_rest_api_plus.py
