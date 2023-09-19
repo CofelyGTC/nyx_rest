7c15897152e54f1cca77ebef9569ae337a783c8a
Julian Fickers <96172826+julianfickerseq@users.noreply.github.com>
2023-09-19 18:05:27 +0200

    working commit

--- auto-docker.sh
+++ auto-docker.sh
@@ -1,8 +1,19 @@
 #/bin/bash
 filename="./sources/nyx_rest_api_plus.py"
+VERSION="4.2.9"
 
-sed -i '' "s#^VERSION=\".*\"#VERSION=\"$1\"#" $filename
+sed -i '' "s/^VERSION=\".*\"/VERSION=\"$VERSION\"/" $filename
 
 
-docker build -t engiecofely/nyx_restapi_8:v$1 -f Dockerfile .
-docker push engiecofely/nyx_restapi_8:v$1
+docker build -t engiecofely/nyx_restapi_8:v$VERSION -f Dockerfile .
+docker push engiecofely/nyx_restapi_8:v$VERSION
+
+### VERSION HISTORY ###
+
+#   4.2.1   JFI Breaking Change Azure Signin
+#   4.2.2   JFI One function for both azure and password
+#   4.2.5   JFI Env redirect url
+#   4.2.7   JFI Automatic UI version read
+#   4.2.9   JFI Added Azure logout logic
+
+
